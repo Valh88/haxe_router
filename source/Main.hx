43a7f1b68170bc3ab53c8ff57ebc25d4a37078b5
Vano <simplmd@gmail.com>
2025-06-02 17:53:25 +0700

    up

--- source/Main.hx
+++ source/Main.hx
@@ -7,43 +7,43 @@ import haxe_router.Factory.RouterFactory;
 import haxe.ui.containers.VBox;
 import haxe.ui.core.Screen;
 
-class Main
-{
-	public static function main()
-	{
-		// Example use route
-		Toolkit.init();
+// class Main
+// {
+// 	public static function main()
+// 	{
+// 		// Example use route
+// 		Toolkit.init();
 
-		var rootView = new VBox();
-		rootView.percentWidth = rootView.percentHeight = 100;
+// 		var rootView = new VBox();
+// 		rootView.percentWidth = rootView.percentHeight = 100;
 
-		var router = RouterFactory.create(rootView);
-		setupRoutes(router);
+// 		var router = RouterFactory.create(rootView);
+// 		setupRoutes(router);
 
-		Screen.instance.addComponent(rootView);
+// 		Screen.instance.addComponent(rootView);
 
-		router.navigate("/");
-	}
+// 		router.navigate("/");
+// 	}
 
-	private static function setupRoutes(router:ARouter):Void
-	{
-		router.addRoute("/", () ->
-		{
-			var view = new HBox();
-			var button = new Button();
-			button.text = "Go to products";
-			button.onClick = function(e)
-			{
-				router.navigate("/products/123", ["category" => "electronics", "sort" => "price"]);
-			};
-			view.addComponent(button);
-			return view;
-		});
+// 	private static function setupRoutes(router:ARouter):Void
+// 	{
+// 		router.addRoute("/", () ->
+// 		{
+// 			var view = new HBox();
+// 			var button = new Button();
+// 			button.text = "Go to products";
+// 			button.onClick = function(e)
+// 			{
+// 				router.navigate("/products/123", ["category" => "electronics", "sort" => "price"]);
+// 			};
+// 			view.addComponent(button);
+// 			return view;
+// 		});
 
-		router.addRoute("/products/:id", () ->
-		{
-			var view = new Label();
-			return view;
-		});
-	}
-}
+// 		router.addRoute("/products/:id", () ->
+// 		{
+// 			var view = new Label();
+// 			return view;
+// 		});
+// 	}
+// }
